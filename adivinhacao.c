db916598105eeec966baaad4fd0c6e66f26a18fa
Tiago Rocha <tiagoferreira.info@gmail.com>
2016-07-19 00:22:56 -0300

    limitar a quantidade de tentativas

--- adivinhacao.c
+++ adivinhacao.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
+#define QUANT_TENTATIVAS_NIVEL_FACIL 20
+#define QUANT_TENTATIVAS_NIVEL_MEDIO 15
+#define QUANT_TENTATIVAS_NIVEL_DIFICIL 6
+
 int main() {
 	
 	//Imprimi o cabeçalho do jogo
@@ -13,16 +17,30 @@ int main() {
     srand(segundos);
 
     int numeroGrande = rand();
-
     int numeroSecreto = numeroGrande % 100;
-
     int chute=-1;
-
     int tentativas =1;
-
    	double pontos = 1000;
 	
-	while(1){
+	int acertou = 0;
+
+	int nivel;
+
+	int numeroDeTentativas;
+
+	printf("Qual o nível de dificuldade? \n");
+	printf("(1) Fácil (2) Médio (3) Difícil\n\n");
+	printf("Escolha: ");
+	scanf("%d",&nivel);
+
+	switch(nivel){
+
+		case 1: numeroDeTentativas =QUANT_TENTATIVAS_NIVEL_FACIL; break;				
+		case 2: numeroDeTentativas = QUANT_TENTATIVAS_NIVEL_MEDIO; break;
+		default: numeroDeTentativas = QUANT_TENTATIVAS_NIVEL_DIFICIL; break;	
+	}
+	
+	for (int i=1; i<=numeroDeTentativas;i++){
 	
 		printf("Tentativa %d\n",tentativas );
 		printf("Qual o seu chute? ");
@@ -34,15 +52,12 @@ int main() {
 	    	continue;
 	    }
 
-	  	int acertou = (chute==numeroSecreto);
+	  	acertou = (chute==numeroSecreto);
 	  	int maior = (chute > numeroSecreto);
 
 	    printf("Seu chute foi %d \n",chute);
 
-		    if(acertou){
-		    	
-		    	printf("Parabéns! Você acertou!\n");
-		    	printf("Jogue de novo, Você é um bom joagador!!\n");
+		    if(acertou){		    			    	
 
 		    	break;
 
@@ -60,8 +75,16 @@ int main() {
 
 	    	pontos = pontos - pontosPerdidos;
 	}
-    
-    printf("Fim de Jogo!\n");
-    printf("Você acertou em %d tentativas\n", tentativas);
-    printf("Total de pontos %.2f\n",pontos );
+
+	printf("Fim de Jogo!\n");
+	if(acertou){
+		
+		printf("Você ganhou!\n");
+		printf("Você acertou em %d tentativas!\n", tentativas);
+        printf("Total de pontos: %.1f\n", pontos);
+        
+	}else{
+
+		printf("Você perdeu, tente de novo!\n");
+	}
 }
