48ac73a6d97ad09fad4e34401c9191b6c78e5702
Tiago Rocha <tiagoferreira.info@gmail.com>
2016-07-20 16:59:35 -0300

    refatoração

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+int enforcou();
+int acertou();
+int adicionaPalavra();
+int chutesErrados();
