c6ea842bd81444d19a79f4221e151247d3480212
Tiago Rocha <tiagoferreira.info@gmail.com>
2016-07-19 21:03:28 -0300

    refatoração

--- adivinhacao.c
+++ adivinhacao.c
@@ -6,22 +6,7 @@
 #define QUANT_TENTATIVAS_NIVEL_MEDIO 15
 #define QUANT_TENTATIVAS_NIVEL_DIFICIL 6
 
-int main() {
-	
-    int segundos = time(0);
-    srand(segundos);
-
-    int numeroGrande = rand();
-    int numeroSecreto = numeroGrande % 100;
-    int chute=-1;
-    int tentativas =1;
-   	double pontos = 1000;
-	
-	int acertou = 0;
-
-	int nivel;
-
-	int numeroDeTentativas;
+void abertura(){
 
 printf("		   \\ \\  ___   __ _  ___                                  		\n");
 printf("		    \\ \\/ _ \\ / _` |/ _ \\                                		\n"); 
@@ -39,12 +24,92 @@ printf("		  /_\\   __| (_)_   _(_)_ __ | |__   __ _  ___ __ _  ___  		\n");
 printf("		 //_\\ / _` | \\ \\ / / | '_ \\| '_ \\ / _` |/ __/ _` |/ _ \\ 		\n");
 printf("		/  _  \\ (_| | |\\ V /| | | | | | | | (_| | (_| (_| | (_) |		\n");
 printf("		\\_/ \\_/\\__,_|_| \\_/ |_|_| |_|_| |_|\\__,_|\\___\\__,_|\\___/ 		\n");
-printf("																		\n\n");												
+printf("																		\n\n");	
+}
 
+void voceAcertou(int tentativas, int pontos){
+
+printf("												\n");
+printf("				OOOOOOOOOOO 					\n");
+printf("	         OOOOOOOOOOOOOOOOOOO 				\n");
+printf("	      OOOOOO  OOOOOOOOO  OOOOOO 			\n");
+printf("	    OOOOOO      OOOOO      OOOOOO 			\n");
+printf("	  OOOOOOOO  #   OOOOO  #   OOOOOOOO 		\n");	
+printf("	 OOOOOOOOOO    OOOOOOO    OOOOOOOOOO 		\n");
+printf("	OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO 		\n");
+printf("	OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO 		\n");
+printf("	OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO 		\n");
+printf("	 OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO 		\n");
+printf("	  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO 		\n");
+printf("	    OOOOO   OOOOOOOOOOOOOOO   OOOO 			\n");	
+printf("	      OOOOOO   OOOOOOOOO   OOOOOO  			\n");
+printf("	         OOOOOO         OOOOOO  			\n");
+printf("	             OOOOOOOOOOOO  					\n");
+printf("												\n");
+
+		printf("Você ganhou!\n");
+		printf("Você acertou em %d tentativas!\n", tentativas);
+        printf("Total de pontos: %.1f\n", pontos);
+
+}
+
+void vocePerdeu(){
+printf("	                          oooo$$$$$$$$$$$$ooooo                  					\n");
+printf("	                      oo$$$$$$$$$$$$$$$$$$$$$$$$oo             						\n");
+printf("	                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$  			\n");
+printf("	   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$ 			\n");	
+printf("	oo $ $ \"$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$ 			\n");
+printf("	\"$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$ 				\n");
+printf("	  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$ 				\n");
+printf("	  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  \"\"\"$$$ 				\n");
+printf("	   \"$$$\"\"\"\"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$ 				\n");
+printf("	    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$o 			\n");
+printf("	   o$$\"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o 			\n");
+printf("	   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\" \"$$$$$$ooooo$$$$o 			\n");
+printf("	  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$ 		\n");
+printf("	  $$$$$$$$\"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$\"\"\"\"\"\"\"\" 				\n");
+printf("	 \"\"\"\"       $$$$    \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"      o$$$  						\n");
+printf("	            \"$$$o     \"\"\"$$$$$$$$$$$$$$$$$$\"$$\"         $$$ 						\n");
+printf("	              $$$o          \"$$\"\"$$$$$$\"\"\"\"           o$$$ 							\n");
+printf("	               $$$$o                                o$$$\"  							\n");
+printf("	                \"$$$$o      o$$$$$$o\"$$$$o        o$$$$  							\n");
+printf("	                  \"$$$$$oo     \"\"$$$$o$$$$$o   o$$$$\"\"            					\n");
+printf("	                     \"\"$$$$$oooo  \"$$$o$$$$$$$$$\"\"\" 								\n");
+printf("	                        \"\"$$$$$$$oo $$$$$$$$$$ 										\n");
+printf("	                                \"\"\"\"$$$$$$$$$$$  									\n");
+printf("	                                    $$$$$$$$$$$$ 									\n");
+printf("	                                     $$$$$$$$$$\"  									\n");
+printf("	                                      \"$$$\"\"\"\" 										\n");
+printf("																						\n");
+printf("																						\n");
+		printf("Você perdeu, tente de novo!\n");
+}
+
+int getNivelDificuldade(){
 	printf("Qual o nível de dificuldade? \n");
 	printf("(1) Fácil (2) Médio (3) Difícil\n\n");
 	printf("Escolha: ");
 	scanf("%d",&nivel);
+}
+
+int main() {
+	
+    int segundos = time(0);
+    srand(segundos);
+
+    int numeroGrande = rand();
+    int numeroSecreto = numeroGrande % 100;
+    int chute=-1;
+    int tentativas =1;
+   	double pontos = 1000;
+	
+	int acertou = 0;
+
+	int nivel = getNivelDificuldade();
+
+	int numeroDeTentativas;
+
+	abertura();										
 
 	switch(nivel){
 
@@ -92,60 +157,9 @@ printf("																		\n\n");
 	printf("Fim de Jogo!\n");
 	if(acertou){
 
-printf("												\n");
-printf("				OOOOOOOOOOO 					\n");
-printf("	         OOOOOOOOOOOOOOOOOOO 				\n");
-printf("	      OOOOOO  OOOOOOOOO  OOOOOO 			\n");
-printf("	    OOOOOO      OOOOO      OOOOOO 			\n");
-printf("	  OOOOOOOO  #   OOOOO  #   OOOOOOOO 		\n");	
-printf("	 OOOOOOOOOO    OOOOOOO    OOOOOOOOOO 		\n");
-printf("	OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO 		\n");
-printf("	OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO 		\n");
-printf("	OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO 		\n");
-printf("	 OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO 		\n");
-printf("	  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO 		\n");
-printf("	    OOOOO   OOOOOOOOOOOOOOO   OOOO 			\n");	
-printf("	      OOOOOO   OOOOOOOOO   OOOOOO  			\n");
-printf("	         OOOOOO         OOOOOO  			\n");
-printf("	             OOOOOOOOOOOO  					\n");
-printf("												\n");
-
-		printf("Você ganhou!\n");
-		printf("Você acertou em %d tentativas!\n", tentativas);
-        printf("Total de pontos: %.1f\n", pontos);
-
+		voceAcertou(tentativas,pontos);
 	}else{
 
-
-
-printf("	                          oooo$$$$$$$$$$$$ooooo                  					\n");
-printf("	                      oo$$$$$$$$$$$$$$$$$$$$$$$$oo             						\n");
-printf("	                   oo$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o         o$   $$ o$  			\n");
-printf("	   o $ oo        o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$o       $$ $$ $$o$ 			\n");	
-printf("	oo $ $ \"$      o$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$o       $$$o$$o$ 			\n");
-printf("	\"$$$$$$o$     o$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$o    $$$$$$$$ 				\n");
-printf("	  $$$$$$$    $$$$$$$$$$$      $$$$$$$$$$$      $$$$$$$$$$$$$$$$$$$$$$$ 				\n");
-printf("	  $$$$$$$$$$$$$$$$$$$$$$$    $$$$$$$$$$$$$    $$$$$$$$$$$$$$  \"\"\"$$$ 				\n");
-printf("	   \"$$$\"\"\"\"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$ 				\n");
-printf("	    $$$   o$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     \"$$$o 			\n");
-printf("	   o$$\"   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$       $$$o 			\n");
-printf("	   $$$    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\" \"$$$$$$ooooo$$$$o 			\n");
-printf("	  o$$$oooo$$$$$  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   o$$$$$$$$$$$$$$$$$ 		\n");
-printf("	  $$$$$$$$\"$$$$   $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$     $$$$\"\"\"\"\"\"\"\" 				\n");
-printf("	 \"\"\"\"       $$$$    \"$$$$$$$$$$$$$$$$$$$$$$$$$$$$\"      o$$$  						\n");
-printf("	            \"$$$o     \"\"\"$$$$$$$$$$$$$$$$$$\"$$\"         $$$ 						\n");
-printf("	              $$$o          \"$$\"\"$$$$$$\"\"\"\"           o$$$ 							\n");
-printf("	               $$$$o                                o$$$\"  							\n");
-printf("	                \"$$$$o      o$$$$$$o\"$$$$o        o$$$$  							\n");
-printf("	                  \"$$$$$oo     \"\"$$$$o$$$$$o   o$$$$\"\"            					\n");
-printf("	                     \"\"$$$$$oooo  \"$$$o$$$$$$$$$\"\"\" 								\n");
-printf("	                        \"\"$$$$$$$oo $$$$$$$$$$ 										\n");
-printf("	                                \"\"\"\"$$$$$$$$$$$  									\n");
-printf("	                                    $$$$$$$$$$$$ 									\n");
-printf("	                                     $$$$$$$$$$\"  									\n");
-printf("	                                      \"$$$\"\"\"\" 										\n");
-printf("																						\n");
-printf("																						\n");
-		printf("Você perdeu, tente de novo!\n");
+		vocePerdeu();
 	}
 }
